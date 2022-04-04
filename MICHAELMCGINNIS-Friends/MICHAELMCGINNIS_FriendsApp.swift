af9bd372fdf57c5dd6f7476a8d7b0b372f79cbcb
Rukosa <mwmcginnis490@gmail.com>
2022-04-04 14:33:57 -0700

    Initial Commit

---
+++ MICHAELMCGINNIS-Friends/MICHAELMCGINNIS_FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MICHAELMCGINNIS_FriendsApp.swift
+//  MICHAELMCGINNIS-Friends
+//
+//  Created by Michael Mcginnis on 4/4/22.
+//
+
+import SwiftUI
+
+@main
+struct MICHAELMCGINNIS_FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
